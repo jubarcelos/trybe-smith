c81d8cf294e1ec65c7a8b5344da922534a30617d
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-05-19 00:51:35 -0300

    Update app
    Input routes, import files and general  updated

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,24 @@
 import express from 'express';
-import getProducts from './Controller/GetProducts';
-import getOrders from './Controller/GetOrders';
-import error from '../middleware/Errors';
+import getProducts from './controllers/GetProducts';
+import getUsers from './controllers/GetUsers';
+import postProducts from './controllers/PostProducts';
+// import getOrders from './controllers/GetOrders';
+// import postOrders from './controllers/PostOrder';
+import postUsers from './controllers/PostUsers';
+import error from './middlewares/Errors';
+import inputValidator from './middlewares/InputValidator';
+import Schema from './schemas';
+import Jwt from './middlewares/jwt';
 
 const app = express();
 
 app.use(express.json());
-
 app.get('/products', getProducts);
-app.get('/orders', getOrders);
+app.post('/products', inputValidator(Schema.input.productPOST), postProducts);
+app.get('/users', getUsers);
+app.post('/users', inputValidator(Schema.input.userPOST), Jwt, postUsers);
+// app.get('/orders', getOrders);
+// app.post('/orders', inputValidator(Schema.input.orderPOST), postOrders);
+
 app.use(error);
 export default app;
