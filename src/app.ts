123a799e3ad648b21303942fe22086294eed7e4a
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-05-19 00:34:53 -0300

    Update
    Setap updated

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import getProducts from './Controller/GetProducts';
+import getOrders from './Controller/GetOrders';
+import error from '../middleware/Errors';
 
 const app = express();
 
 app.use(express.json());
 
+app.get('/products', getProducts);
+app.get('/orders', getOrders);
+app.use(error);
 export default app;
